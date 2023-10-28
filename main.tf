590a2f656dfffb6a062d5c727cf5333f6da00880
kumariseait <93650590+kumariseait@users.noreply.github.com>
2023-10-28 12:18:35 +0530

    ec2 instance creation

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "app_server" {
+
+  ami           = var.ami
+
+  instance_type = var.inst_type
+
+  availability_zone = var.inst_az
+
+  key_name = "kv-key"
+
+  tags = {
+    Name = "myec2-inst1"
+  }
+}
